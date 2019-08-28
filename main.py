31ce4ad94927ed5c07c8ba6a76ad10f5f2cfc30c
jackofnotrades <mike.cutshaw@hotmail.com>
2019-08-28 14:58:34 -0500

    took more from config

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from vcloud import vcloud
 import configparser
+import time
 
 config = configparser.ConfigParser()
 config.read('renew.conf')
 filters = config['Renew']['Filters'].split(',')
 vcloud = vcloud(config)
-defsec = vcloud.getCatalog('DefSec')
+defsec = vcloud.getCatalog(config['Renew']['Catalog'])
+vdc = vcloud.getVdc(config['Renew']['Vdc'])
+
 for filt in filters:
     templates = defsec.getTemplates(filter=filt)
     for template in templates:
-        template.renew()
-        print(template.name)
+            template.deploy(vdc, name='testingdeploy_1'+vdc.id)
