8156b1f8c1c2d0718955dfdb71de17a56f6220c2
mcutshaw <trash@jackofnotrades.org>
2019-09-06 13:27:34 -0500

    added multiprocessing

--- deploy.py
+++ deploy.py
@@ -1,28 +1,48 @@
 from vcloud import vcloud
 import configparser
 import time
+from multiprocessing import Pool
 
-config = configparser.ConfigParser()
-config.read('vcloud.conf')
-filters = config['Extra']['Filters'].split(',')
-vcloud = vcloud(config)
-catalog = vcloud.getCatalog(config['Extra']['Catalog'])
-vdc = vcloud.getVdc(config['Extra']['Vdc'])
-org = vcloud.getOrg(config['Main']['Org'])
-role = org.getRole(config['Deploy']['Role'])
 
-templates = catalog.getTemplates(filter='Blank')
-with open('users.txt','r') as f:
-    users = f.read().split('\n')
-for struser in users:
-    for template in templates:
-        user = org.getUser(struser, role=role)
-        if user is None:
-            continue
-        vapp = template.deploy(vdc,name=user.name+'_'+template.name)
-        if vapp is None:
-            continue
-        endvapp = vapp.changeOwner(user, timeout=300, checkTime=5)
-        if endvapp is not None:
-            print(user.name+'_'+template.name, 'deployed successfully deployed to', user.name)
+def deployToUser(tup):
+    user = tup[0]
+    template = tup[1]
+    vdc = tup[2]
+    if user is None:
+        return
+    vapp = template.deploy(vdc,name=user.name+'_'+template.name)
+    if vapp is None:
+        return
+    endvapp = vapp.changeOwner(user, timeout=300, checkTime=5)
+    if endvapp is not None:
+        print(user.name+'_'+template.name, 'deployed successfully deployed to', user.name)
+
+if __name__ == '__main__':
+    start_time = time.time()
+    config = configparser.ConfigParser()
+    config.read('vcloud.conf')
+    filters = config['Extra']['Filters'].split(',')
+    vcloud = vcloud(config)
+    catalog = vcloud.getCatalog(config['Extra']['Catalog'])
+    vdc = vcloud.getVdc(config['Extra']['Vdc'])
+    org = vcloud.getOrg(config['Main']['Org'])
+    role = org.getRole(config['Deploy']['Role'])
+
+    p = Pool(3)
+
+
+    templates = catalog.getTemplates(filter='Blank')
+    template = templates[0]
+
+    with open('users.txt','r') as f:
+        users = f.read().split('\n')
+    
+    l = []
+    for user in users:
+        user = org.getUser(user, role=role)
+        l.append((user, template, vdc))
+
+    p.map(deployToUser, l)
+    print("Deploy took", (time.time() - start_time), "seconds.")
+    
 
